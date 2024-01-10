dff69f00c425d8b8d40ff2a8ae309c0b8d6d9bf6
juliamanska <julia.ma@onet.com.pl>
2024-01-10 10:35:39 +0100

    Add multiple droplets imitating rain

--- remove-droplet.js
+++ remove-droplet.js
@@ -4,32 +4,48 @@ canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 let ctx = canvas.getContext("2d");
 
-let y = 20;
-
-function Droplet(x, y) {
+function Droplet(x, y, dy) {
   this.x = x;
   this.y = y;
+  this.dy = dy;
 
   this.draw = function () {
     ctx.beginPath();
-    ctx.moveTo(100, this.y);
-    ctx.bezierCurveTo(65, this.y + 40, 140, this.y + 40, 100, this.y);
+    ctx.moveTo(this.x, this.y);
+    ctx.bezierCurveTo(
+      this.x - 35,
+      this.y + 40,
+      this.x + 40,
+      this.y + 40,
+      this.x,
+      this.y
+    );
 
     ctx.closePath();
     ctx.fillStyle = "blue";
     ctx.fill();
     ctx.stroke();
-    this.y += 1;
+    this.y += dy;
   };
 }
+let dropletArray = [];
 
-let droplet = new Droplet(100, y);
-droplet.draw();
+for (let i = 0; i < 100; i++) {
+  let x = Math.random() * innerWidth;
+  let y = Math.random() * innerHeight;
+  let dy = (Math.random() + 2) * 1.3;
+  dropletArray.push(new Droplet(x, y, dy));
+  console.log(dy);
+}
 
 function animate() {
   requestAnimationFrame(animate);
   ctx.clearRect(0, 0, innerWidth, innerHeight);
-  droplet.draw();
+  for (let i = 0; i < dropletArray.length; i++) {
+    dropletArray[i].draw();
+  }
 }
 
 animate();
+
+console.log(dropletArray);
